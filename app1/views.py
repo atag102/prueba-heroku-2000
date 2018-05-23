f071d5438e5ff246093e61d957f078cbac39a838
atag102 <atag102@gmail.com>
2018-05-23 07:39:54 -0400

    Actuaalizacion para la conccion con la base de datos en mlab

--- app1/views.py
+++ app1/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse 
+from pymongo import *
 
 def home(request):
+	client=MongoClient ('mongodb://atag10:C8720413@ds221990.mlab.com:21990/amv')
+	 db  =  client.amv
+	 colección  =  db.pruebaConeccion
+	 D3={"nombre": "Jorge","Edad":55,"Apellido":"Nohra","Nacionalidad": "Livaneza"}
+	 colección.insert_one(D3).inserted_id
 	return render(request,'app1/home.html')
